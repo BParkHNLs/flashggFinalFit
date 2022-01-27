a96ef2a07613363b1da42881903345e879c03810
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2022-01-27 19:05:44 +0100

    latest config version

--- Background/config_mg.py
+++ Background/config_mg.py
@@ -3,10 +3,12 @@
 backgroundScriptCfg = {
   
   # Setup
-  'inputWSDir':'/work/mratti/BParkingNano/Oct_CMSSW_10_2_15/CMSSW_10_2_15/src/PhysicsTools/BHNLAnalysis/exampleWS/ws/', # location of 'allData.root' file
+  'inputWSDir':'/work/mratti/BParkingNano/Oct_CMSSW_10_2_15/CMSSW_10_2_15/src/PhysicsTools/BHNLAnalysis/exampleWS/wsAnalysis/', # location of 'allData.root' file
+  #'inputWSDir':'/work/anlyon/flashgg/CMSSW_10_2_13/src/flashggFinalFit/Background/test/mine/data_cats_sel_large/', # location of 'allData.root' file
+  #'inputWSDir':'/work/anlyon/flashgg/CMSSW_10_2_13/src/flashggFinalFit/Background/test/mine/data_cats_sel/', # location of 'allData.root' file
   'cats':'auto', # auto: automatically inferred from input ws
   'catOffset':0, # add offset to category numbers (useful for categories from different allData.root files)  
-  'ext':'testMG', # extension to add to output directory
+  'ext':'testMGRealData', # extension to add to output directory
   'year':'2018', # Use combined when merging all years in category (for plots)
 
   # Job submission options
